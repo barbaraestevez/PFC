2162e4b498e525dc0d371b05deda8e0eb8f6c8d7
barbaraestevez <barbara.estevez@gmail.com>
2023-12-21 13:40:25 +0100

    implemented aggregate logic to update from our database

--- ShopServer/controllers/userController.js
+++ ShopServer/controllers/userController.js
@@ -55,11 +55,66 @@ exports.addSalesByUser = async (req, res) => {
   const { collection, client } = connectCollection("shop", "Users");
   try {
     if (await authLogic.checkRBAC(req, ["Seller"])) {
-      const { userId, productList } = req.body;
-      const filter = new ObjectId(userId);
-      const update = { $addToSet: { store: { $each: productList } } };
 
-      const user = await collection.updateOne(filter, update);
+      const { userId, userStore, productList } = req.body;
+
+
+      const result = await collection.updateOne(
+        { _id: new ObjectId(userId) },
+        {
+          $addToSet: { store: { $each: productList } } //$push
+        });
+
+      const cursor = collection.aggregate([
+        {
+          $match: {
+            _id: new ObjectId(userId)
+          }
+        },
+        {
+          $unwind: "$store"
+        },
+        {
+          $group: {
+            _id: {
+              name: "$name",
+              product_name: "$store.product_name"
+            },
+            username: {$first: "$username"},
+            email: {$first: "$email"},
+            role: {$first: "$role"},
+            password: {$first: "$password"},
+            totalQty: {$sum: "$store.qty"},
+            avgPrice: {$avg: "$store.price"}
+          }
+        },
+        {
+          $group: {
+            _id: "$_id.name",
+            email: {$first: "$email"},
+            role: {$first: "$role"},
+            store: {
+              $push: {
+                product_name: "$_id.name",
+                qty: "$totalQty",
+                price: "$avgPrice"
+              }
+            }
+          }
+        }
+      ]);
+      let document = {};
+      for await (const doc of cursor) {
+        document = doc;
+      }
+      const resilt = await collection.updateOne(
+        { _id: new ObjectId(userId) },
+        {
+          $set: document
+        }
+      );
+
+      
       if (result.matchedCount === 1 && result.modifiedCount === 1) {
         res.json({ msg: "Productos añadidos a tu tienda", success: true });
       } else {
@@ -76,4 +131,33 @@ exports.addSalesByUser = async (req, res) => {
   } finally {
     await client.close();
   }
-};
+}
+
+/*
+async function findUserById(req) {
+  const { collection, client } = connectCollection ('shop', 'Users');
+  try {
+    return await collection.findOne({ _id: new ObjectId(req.userId)});
+  }
+  catch(error) {
+
+  }
+  finally {
+    await client.close();
+  }
+}
+
+function combineProduct(currentList, newList) {
+  const combinedList = currentList.concat(newList).reduce((result, current) => {
+    const existingProduct = result.find(
+      product => product.name === current.name && product.price === current.price);
+
+  if (existingProduct) {
+    existingProduct.qty += current.qty;
+  } else {
+    result.push(current);
+  }
+  })
+  return combinedList;
+}
+*/
